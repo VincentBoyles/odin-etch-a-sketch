2fc4e29fbd513faad77ce30f7f2780bd2b3ca666
VincentBoyles <boylesvincentmanuel@gmail.com>
2024-06-24 15:29:28 +0800

    finally figured out the how to auto resize grid in a container after 10 decades

--- javascript.js
+++ javascript.js
@@ -9,6 +9,7 @@ title.setAttribute('id', 'title')
 title.textContent = 'Etch-a-Sketch';
 container.appendChild(title);
 
+//Buttons
 
 //Container border for the grid
 const gridBorder = document.createElement('div');
@@ -16,9 +17,11 @@ gridBorder.setAttribute('id', 'gridBorder');
 container.appendChild(gridBorder);
 
 // Creation of grid boxes
-for (let i = 1; i < 257; i++) { 
+for (let i = 0; i <256; i++) { 
 let gridBox = document.createElement('div');
-gridBox.setAttribute('id',  'gridBox');
+gridBox.setAttribute('id', 'gridBox');
+gridBox.height = `${700/numberofGrid}px`
+gridBox.width = `${700/numberofGrid}px`
 gridBorder.appendChild(gridBox);
 }
 
