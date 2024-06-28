ef2d557bfae7f5da74619ae052f4b83d9a3d762c
VincentBoyles <boylesvincentmanuel@gmail.com>
2024-06-28 21:26:49 +0800

    added set grid button to function properly

--- javascript.js
+++ javascript.js
@@ -21,47 +21,40 @@ numberOfGrids.textContent = 'Number of Grids';
 buttonContainer.appendChild(numberOfGrids);
 
 
-// Creation of New Grids
+// Creation of new grids from button
 numberOfGrids.addEventListener('click', function() {
-    let newSquares = Number(prompt('Enter a number between 16-100'));
-    if (newSquares === null) {
-        return;
-    } else if (newSquares < 16 || newSquares > 100 || isNaN(newSquares)) {
-        alert('Wrong input value!');
+    let newSquares = Number(prompt('Enter a number between 16 to 100: '))
+    if (newSquares < 16 || newSquares > 100 || isNaN(newSquares)) {
+        alert('Error: Invalid Value')
     } else {
-            let newGrid = squares*squares;
-            for (let i = 0; i <newGrid; i++) { 
-            const newBox = document.createElement('div');
-            newBox.setAttribute('id', 'newBox');
-            let newRowColumn = (700/newSquares);
-            newBox.style.height = `${newRowColumn}px`;
-            newBox.style.width =  `${newRowColumn}px`;
-            gridBorder.replaceChild(newBox);
-        }
+        createGrid(newSquares);
     }
 });
+
 
 //Container border for the grid
 const gridBorder = document.createElement('div');
 gridBorder.setAttribute('id', 'gridBorder');
 container.appendChild(gridBorder);
 
-// Creation of grid boxes
-let squares = 16;
-let totalGrid = squares*squares;
+
+// Function for creating squares
+function createGrid (squares) {
+    gridBorder.innerHTML='';
+    let totalGrid = squares*squares;
 for (let i = 0; i <totalGrid; i++) { 
 const gridBox = document.createElement('div');
 gridBox.setAttribute('id', 'gridBox');
-let gridRowColumn = (700/squares);
+let gridRowColumn = Math.abs(700/squares);
 gridBox.style.height = `${gridRowColumn}px`;
 gridBox.style.width =  `${gridRowColumn}px`;
 
-// Mouse over event
 gridBox.addEventListener('mouseover', event => {
     event.target.style.backgroundColor = 'black';
 });
 gridBorder.appendChild(gridBox);
 };
+}
 
-
-
+//Default creation of 16x16
+createGrid(16);
