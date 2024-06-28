910ef4514417c1b24c8078b2185c5462107869f3
VincentBoyles <boylesvincentmanuel@gmail.com>
2024-06-29 00:53:52 +0800

    finished project :D!

--- javascript.js
+++ javascript.js
@@ -6,30 +6,13 @@ document.body.appendChild(container);
 // Container for the title element
 const title = document.createElement('h1');
 title.setAttribute('id', 'title')
-title.textContent = 'Etch-a-Sketch';
+title.textContent = 'Etch-a-Sketch :D';
 container.appendChild(title);
 
 //Button Container
 const buttonContainer = document.createElement('div');
 buttonContainer.setAttribute('id', 'buttonContainer');
 container.appendChild(buttonContainer);
-
-
-// Grid Button
-const numberOfGrids = document.createElement('button');
-numberOfGrids.textContent = 'Number of Grids';
-buttonContainer.appendChild(numberOfGrids);
-
-
-// Creation of new grids from button
-numberOfGrids.addEventListener('click', function() {
-    let newSquares = Number(prompt('Enter a number between 16 to 100: '))
-    if (newSquares < 16 || newSquares > 100 || isNaN(newSquares)) {
-        alert('Error: Invalid Value')
-    } else {
-        createGrid(newSquares);
-    }
-});
 
 
 //Container border for the grid
@@ -48,13 +31,57 @@ gridBox.setAttribute('id', 'gridBox');
 let gridRowColumn = Math.abs(700/squares);
 gridBox.style.height = `${gridRowColumn}px`;
 gridBox.style.width =  `${gridRowColumn}px`;
-
 gridBox.addEventListener('mouseover', event => {
-    event.target.style.backgroundColor = 'black';
+    colorChoice = Math.floor(Math.random() * 7);
+    switch (colorChoice) {
+        case 1:
+            colorChoice = 'red';
+            break;
+        case 2:
+            colorChoice = 'orange';
+            break;
+        case 3:
+            colorChoice = 'yellow';
+            break;
+        case 4:
+            colorChoice = 'green';
+            break;
+        case 5:
+            colorChoice = 'blue';
+            break;
+        case 6:
+            colorChoice = 'indigo';
+            break;
+        case 7:
+            colorChoice = 'violet';
+            break;
+    }
+    event.target.style.backgroundColor = colorChoice;
 });
 gridBorder.appendChild(gridBox);
 };
 }
 
+
 //Default creation of 16x16
 createGrid(16);
+
+
+// Grid Button
+const numberOfGrids = document.createElement('button');
+numberOfGrids.textContent = 'Change number of squares';
+buttonContainer.appendChild(numberOfGrids);
+
+
+// Creation of new grids from button
+numberOfGrids.addEventListener('click', function() {
+    let newSquares = (prompt('Enter a number between 16 to 100: '))
+    if (newSquares === null) {
+        return;
+    } else if (newSquares < 16 || newSquares > 100 || isNaN(newSquares))  {
+        alert('Error: Invalid Value')
+    } else {
+        createGrid(newSquares);
+    }
+});
+
